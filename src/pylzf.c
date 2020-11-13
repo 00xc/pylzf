59925fdb4bb3a96af9e86793db3635c06eb38512
carlos <48725664+00xc@users.noreply.github.com>
2020-11-13 17:21:13 +0100

    Release GIL during (de)compression and improve cleanup code readability

--- src/pylzf.c
+++ src/pylzf.c
@@ -7,6 +7,11 @@
 #define MAX_COMPRESS_OVERSIZE_SAFE	2
 #define MAX_DECOMPRESS_OVERSIZE 	1.4
 
+#define THREADSAFE_CLEANUP(ptr, buf) \
+	if (ptr) { free(ptr); } \
+	Py_BLOCK_THREADS \
+	PyBuffer_Release(buf); \
+
 static PyObject* pylzf_compress(PyObject* self, PyObject* args) {
 	Py_buffer in_obj;
 	PyObject* out_obj;
@@ -17,25 +22,29 @@ static PyObject* pylzf_compress(PyObject* self, PyObject* args) {
 		return NULL;
 	}
 
-	/* Estimate maximum compression output length */
-	if (in_obj.len > 7) {
-		aux = lround(in_obj.len * MAX_COMPRESS_OVERSIZE);
-	} else {
-		aux = lround(in_obj.len * MAX_COMPRESS_OVERSIZE_SAFE);
-	}
+	/* GIL-free section */
+	Py_BEGIN_ALLOW_THREADS
 
-	if ( (out = malloc(aux)) == NULL ) {
-		PyBuffer_Release(&in_obj);
-		return PyErr_NoMemory();
-	}
+		/* Estimate maximum compression output length */
+		if (in_obj.len > 7) {
+			aux = lround(in_obj.len * MAX_COMPRESS_OVERSIZE);
+		} else {
+			aux = lround(in_obj.len * MAX_COMPRESS_OVERSIZE_SAFE);
+		}
 
-	/* Data compression */
-	if ((aux = lzf_compress(in_obj.buf, in_obj.len, out, aux)) == 0) {
-		free(out);
-		PyBuffer_Release(&in_obj);
-		PyErr_SetNone(PyExc_IOError);
-		return NULL;
-	}
+		if ( (out = malloc(aux)) == NULL ) {
+			THREADSAFE_CLEANUP(NULL, &in_obj)
+			return PyErr_NoMemory();
+		}
+
+		/* Data compression */
+		if ((aux = lzf_compress(in_obj.buf, in_obj.len, out, aux)) == 0) {
+			THREADSAFE_CLEANUP(out, &in_obj)
+			PyErr_SetNone(PyExc_IOError);
+			return NULL;
+		}
+
+	Py_END_ALLOW_THREADS
 	PyBuffer_Release(&in_obj);
 
 	/* Build output object */
@@ -52,28 +61,33 @@ static PyObject* pylzf_decompress(PyObject* self, PyObject* args) {
 	Py_buffer in_obj;
 	PyObject* out_obj;
 	unsigned char* out;
-	unsigned int ret;
+	unsigned int aux;
 
 	if (!PyArg_ParseTuple(args, "y*", &in_obj)) {
 		return NULL;
 	}
 
-	if ( (out = malloc(in_obj.len * MAX_DECOMPRESS_OVERSIZE)) == NULL ) {
-		PyBuffer_Release(&in_obj);
-		return PyErr_NoMemory();
-	}
+	/* GIL-free section */
+	Py_BEGIN_ALLOW_THREADS
 
-	/* Data decompression */
-	if ((ret = lzf_decompress(in_obj.buf, in_obj.len, out, in_obj.len * MAX_DECOMPRESS_OVERSIZE)) == 0) {
-		free(out);
-		PyBuffer_Release(&in_obj);
-		PyErr_SetNone(PyExc_IOError);
-		return NULL;
-	}
+		aux = in_obj.len * MAX_DECOMPRESS_OVERSIZE;
+
+		if ( (out = malloc(aux)) == NULL ) {
+			THREADSAFE_CLEANUP(NULL, &in_obj)
+			return PyErr_NoMemory();
+		}
+
+		if ((aux = lzf_decompress(in_obj.buf, in_obj.len, out, aux)) == 0) {
+			THREADSAFE_CLEANUP(out, &in_obj)
+			PyErr_SetNone(PyExc_IOError);
+			return NULL;
+		}
+
+	Py_END_ALLOW_THREADS
 	PyBuffer_Release(&in_obj);
 
 	/* Build output object */
-	if ( (out_obj = Py_BuildValue("y#", out, ret)) == NULL ) {
+	if ( (out_obj = Py_BuildValue("y#", out, aux)) == NULL ) {
 		free(out);
 		return NULL;
 	}
